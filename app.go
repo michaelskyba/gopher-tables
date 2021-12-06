6822b82867d9ef8ef5260e4241a78a4853b829d6
michaelskyba <michaelskyba1411@gmail.com>
2021-12-06 17:18:07 -0500

    add lobby handler

--- app.go
+++ app.go
@@ -37,10 +37,17 @@ func register_handler(writer http.ResponseWriter, request *http.Request) {
 	page.Execute(writer, template_values{})
 }
 
+// Lobby
+func lobby_handler(writer http.ResponseWriter, request *http.Request) {
+	page, _ := template.ParseFiles("html/lobby.html")
+	page.Execute(writer, template_values{})
+}
+
 func main() {
 	http.HandleFunc("/", home_handler)
 	http.HandleFunc("/login/", login_handler)
 	http.HandleFunc("/register/", register_handler)
+	http.HandleFunc("/lobby/", lobby_handler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
