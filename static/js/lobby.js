c3f92908e788660a77b234a259d062fe5edb4acc
michaelskyba <michaelskyba1411@gmail.com>
2021-12-17 16:12:11 -0500

    functions of lobby buttons

---
+++ static/js/lobby.js
@@ -0,0 +1,9 @@
+// "Refresh"
+document.getElementById("refresh").onclick = function() {
+	location.reload()
+}
+
+// "Create game"
+document.getElementById("create").onclick = function() {
+	window.location.replace("/create/")
+}
