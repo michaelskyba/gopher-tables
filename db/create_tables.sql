282200125c0d3614af9f0e8bf24f97dc944d3609
michaelskyba <michaelskyba1411@gmail.com>
2021-12-17 17:36:56 -0500

    add remaining tables for the actual game

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -14,3 +14,29 @@ VALUES
 	('Michael Skyba', '1234567890'),
 	('Linus Torvalds', 'Hunter1'),
 	('password', 'password');
+
+CREATE TABLE games (
+	id			INT AUTO_INCREMENT NOT NULL,
+	name		VARCHAR(128) NOT NULL,
+	password	VARCHAR(128) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO games
+	(name, password)
+VALUES
+	('you are going to lose', '');
+
+CREATE TABLE players (
+	id			INT AUTO_INCREMENT NOT NULL,
+	game_id		INT NOT NULL,
+	user_id		INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+-- So Linus is joining the 'you are going to lose game'
+-- Since he's the only player, he must have supposedly been the host
+INSERT INTO players
+	(game_id, user_id)
+VALUES
+	(0, 1);
