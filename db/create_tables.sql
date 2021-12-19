7d461043f2cb94238a508665cc528b08fee73b24
michaelskyba <michaelskyba1411@gmail.com>
2021-12-18 19:01:05 -0500

    default wins for accounts SQL

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE accounts (
 	id			INT AUTO_INCREMENT NOT NULL,
 	username	VARCHAR(128) NOT NULL,
 	password	VARCHAR(255) NOT NULL,
-	wins		INT NOT NULL,
+	wins		INT DEFAULT 0,
 	PRIMARY KEY (`id`)
 );
 
