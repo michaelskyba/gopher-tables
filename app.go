7cb67eee95dab5971776c2b6f85491fe0d6f4724
michaelskyba <michaelskyba1411@gmail.com>
2022-01-05 14:24:26 -0500

    update progress in /answer/

--- app.go
+++ app.go
@@ -428,6 +428,7 @@ func progress_handler(writer http.ResponseWriter, request *http.Request, db *sql
 // API for the /play/ client to send requests to with AJAX
 // This is used when answering questions - you submit your answer here to check if it
 // was right. If it was, your progress will be updated
+// Valid: /answer/your_answer_here/
 func answer_handler(writer http.ResponseWriter, request *http.Request, db *sql.DB) {
 
 	// TODO: Return error if the URL is invalid
@@ -454,8 +455,9 @@ func answer_handler(writer http.ResponseWriter, request *http.Request, db *sql.D
 		fmt.Fprintln(writer, "Game over!")
 	}
 
-	fmt.Fprintln(writer, user_id)
-	fmt.Fprintln(writer, progress)
+	_, err = db.Exec("UPDATE players SET progress = ? WHERE user_id = ?",
+	                  progress, user_id)
+	handle(err)
 }
 
 // Create game form submission URL endpoint
