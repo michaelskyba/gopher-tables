bf4761459bef126eea711230082fa71a1b416e42
michaelskyba <michaelskyba1411@gmail.com>
2022-01-06 08:48:13 -0500

    answer.js: provide game ID and answer to /answer/

--- static/js/play/answer.js
+++ static/js/play/answer.js
@@ -1,11 +1,15 @@
 let answer_request = new XMLHttpRequest()
+let answer_input = document.getElementById("answer")
 
 document.onkeydown = function (e) {
 	e = e || window.event
 
 	// Pressed Enter
 	if (e.keyCode == 13) {
-		answer_request.open("POST", "/answer/", true)
+		let answer = answer_input.value
+		answer_input.value = ""
+
+		answer_request.open("POST", `/answer/${game_id}/${answer}/`, true)
 		answer_request.send()
 	}
 }
